67ddec03ea97f68f24904df38b19d658aa7c7921
RobbinZhu <robbin.zhu@uuabc.com>
2018-12-24 20:25:48 +0800

    uodate html

--- common/playground.js
+++ common/playground.js
@@ -28,18 +28,29 @@
     });
 
     function initEditor(editor) {
-        const iframe = document.querySelector('iframe')
 
         function refresh() {
-            try {
+            const iframe = document.createElement('iframe')
+            iframe.setAttribute('border', 0)
+            iframe.onload = function() {
+                var style = document.createElement('style')
+                style.type = 'text/css'
+                style.innerHTML = `
+                body,html{ background: #efefef;margin:0;padding:0 }
+                canvas{margin:0;padding:0}
+                `
+                this.contentDocument.body.appendChild(style)
                 const text = editor.getValue()
                 var script = iframe.contentDocument.createElement("script")
                 script.setAttribute('type', 'module')
                 script.textContent = text
-                iframe.contentDocument.body.appendChild(script)
-            } catch (e) {
-                console.log("Error from compilation: " + e + "  " + (e.stack || ""));
+                this.contentDocument.body.appendChild(script)
             }
+
+            const container = document.querySelector('#iframe')
+            container.innerHTML = ''
+            container.appendChild(iframe)
+
         }
         document.querySelector('#execute').addEventListener('click', refresh, false)
         refresh()
